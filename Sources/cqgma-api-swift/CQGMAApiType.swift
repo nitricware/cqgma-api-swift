5bdca2662c1098990848dfb544783632c39709f5
Kurt Frey <office@nitricware.com>
2024-08-26 13:56:40 +0200

    initial push

---
+++ Sources/cqgma-api-swift/CQGMAApiType.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kurt Frey on 26.08.24.
+//
+
+import Foundation
---
+++ Sources/cqgma-api-swift/CQGMASpots.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kurt Frey on 21.08.24.
+//
+
+import Foundation
