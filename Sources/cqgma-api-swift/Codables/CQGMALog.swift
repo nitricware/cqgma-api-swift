d2913af04ae390320d3347f402357a258f8f8450
Kurt Frey <office@nitricware.com>
2024-09-09 15:00:49 +0200

    typo

--- Sources/cqgma-api-swift/Codables/CQGMALog.swift
+++ Sources/cqgma-api-swift/Codables/CQGMALog.swift
@@ -4,7 +4,7 @@ public struct CQGMALog: CQGMAAPIPayload {
     public let USER: String
     public let PSWD: String
     public let DUMP: Int
-    public  let LIVE: Int
+    public let LIVE: Int
     public let LOGC: Int
     public let QSO: [CQGMAQSO]
 }
