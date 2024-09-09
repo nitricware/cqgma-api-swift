836703be0361d2a0c1d48694e1d511f865315ef3
Kurt Frey <office@nitricware.com>
2024-09-09 15:00:21 +0200

    add enums for properties that conform to ADIF specifications

---
+++ Sources/cqgma-api-swift/ENUM/ADIFPropagation.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+public enum ADIFPropagation: String {
+    case AS = "AS"
+    case AUE = "AUE"
+    case AUR = "AUR"
+    case BS = "BS"
+    case ECH = "ECH"
+    case EME = "EME"
+    case ES = "ES"
+    case F2 = "F2"
+    case FAI = "FAI"
+    case GWAVE = "GWAVE"
+    case INTERNET = "INTERNET"
+    case ION = "ION"
+    case IRL = "IRL"
+    case LOS = "LOS"
+    case MS = "MS"
+    case RPT = "RPT"
+    case RS = "RS"
+    case SAT = "SAT"
+    case TEP = "TEP"
+    case TR = "TR"
+}
